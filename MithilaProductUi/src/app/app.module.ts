460e4613d15f7a78f0f401e04a34ca53064d62c6
PallabiPrasad <49730970+PallabiPrasad@users.noreply.github.com>
2019-04-20 02:47:37 +0530

    AboutUs
    Did not change the footer in About Us page

--- MithilaProductUi/src/app/app.module.ts
+++ MithilaProductUi/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CarouselPanelComponent } from './home/carousel-panel/carousel-panel.component';
 import { FooterComponent } from './home/footer/footer.component';
+import { AboutComponent } from './About/about/about.component';
+
+const routes: Routes = [
+  {path: 'Home', component: CarouselPanelComponent},
+  {path: 'About', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselPanelComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
